5f0e22e3a16fa33f3fc3a0b9c80e2d6e6ccdae10
AXE-L <fhq8800@gmail.com>
2024-07-01 13:25:07 +0800

    更新自己

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <script async src="https://sy.952727.xyz/script.js" data-website-id="f79a6ab5-d844-4b3a-bdc1-967887001820"></script>
       </head>
       <body>
         {children}
--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -403,7 +403,7 @@ const cn = {
     Revert: "恢复上下文",
   },
   Plugin: {
-    Name: "插件",
+    Name: "探索更多好玩 ！！",
   },
   FineTuned: {
     Sysmessage: "你是一个助手",
